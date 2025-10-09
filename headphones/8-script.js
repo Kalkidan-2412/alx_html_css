b7ad27b2dbe096e4e74d48b75453a998e24921c9
kalkidan Ayalew <ayalewkalkidan48@gmail.com>
2025-10-09 23:21:36 +0300

    Create 8-script.js

---
+++ headphones/8-script.js
@@ -0,0 +1,7 @@
+// 8-script.js
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+});
